c7da844a71f7d1d5b049128c9cb57d893ad42a32
jiadchen <jiadchen@redhat.com>
2025-02-21 15:12:05 +0900

    fix IT and E2E

--- integration-test/sample-app/src/test/java/it/example/demo/SampleAppTest.java
+++ integration-test/sample-app/src/test/java/it/example/demo/SampleAppTest.java
@@ -21,7 +21,7 @@ public class SampleAppTest {
   @Test
   public void sayHelloTest() {
     // Arrange
-    String expected = "Hello, openshift! version:1.0.49";
+    String expected = "Hello, openshift! version:1.0.49 PUSH_ENABLED:not set";
     int expectedStatusCode = 200;
 
     // Act
